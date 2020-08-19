0a35a862ac1f784d85fd5c58e97f3147b331d5b0
JoãoAlves <joao.alves@casamineira.com.br>
2020-08-19 15:30:27 -0300

    Atualizando pwd relativo

--- portal-diario.sh
+++ portal-diario.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ssh -f -o ExitOnForwardFailure=yes -N -L 4321:portal.cehhbd9xyiuz.us-east-1.rds.amazonaws.com:3306 ubuntu@3.219.235.103
 pid=$(pgrep -f '4321:')
-for consulta in "consultas/portal/*.sql"
+RAIZ="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+for consulta in $RAIZ/consultas/portal/*.sql
 do
+    hoje=$(date +"%d-%m-%Y")
     arquivo=$(echo $consulta | grep -Eo '([a-z0-9-]+).sql$' | cut -d. -f1)
-    cat $consulta | mysql -P 4321 -u casamineira_leitura -h 127.0.0.1 --password='PJx7&4i$DYb&ZEpf' casamineira_portal > relatorios/portal/$arquivo.csv
+    nome=$arquivo'_('$hoje')'
+    cat $consulta | mysql -P 4321 -u casamineira_leitura -h 127.0.0.1 --password='PJx7&4i$DYb&ZEpf' casamineira_portal > $RAIZ/relatorios/portal//$arquivo/$nome.csv
 done
 kill $pid
--- sistema-diario.sh
+++ sistema-diario.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 ssh -f -o ExitOnForwardFailure=yes -N -L 1234:app.cehhbd9xyiuz.us-east-1.rds.amazonaws.com:3306 ubuntu@3.220.162.0 
 pid=$(pgrep -f '1234:')
-for consulta in /home/joaoalves/Projetos/scripts-sql/consultas/sistema/*.sql
+RAIZ="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+for consulta in $RAIZ/consultas/sistema/*.sql
 do
+    hoje=$(date +"%d-%m-%Y")
     arquivo=$(echo $consulta | grep -Eo '([a-z0-9-]+).sql$' | cut -d. -f1)
-    cat $consulta | mysql -P 1234 -u casamineiraimoveis_leitura -h 127.0.0.1 --password='ie9#NnXL343^7M7*' casamineira > /home/joaoalves/Projetos/scripts-sql/relatorios/sistema/$arquivo.csv
-    echo $consulta
+    nome=$arquivo'_('$hoje')'
+    cat $consulta | mysql -P 1234 -u casamineiraimoveis_leitura -h 127.0.0.1 --password='ie9#NnXL343^7M7*' casamineira > $RAIZ/relatorios/sistema/$arquivo/$nome.csv
+    echo $nome
 done
 kill $pid
